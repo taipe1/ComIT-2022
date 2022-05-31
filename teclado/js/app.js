57819542f0342c61126d4159739a2e8416e03e18
Andy Taipe <ataipe@unitru.edu.pe>
2022-05-31 02:39:30 -0500

    teclado terminado

--- teclado/js/app.js
+++ teclado/js/app.js
@@ -2,27 +2,34 @@
 let pantalla = document.getElementById('pantalla')
 let linea1 = 'qwertyuiop';
 let linea2 = 'asdfghjklñ';
-let linea3 = 'zxcvbnm';
+let linea3 = 'zxcvbnm,.-';
 
 for(let ite of linea1){
-    document.write('<input type="button" class="alpha" value="'+ite+'" onclick="muestra(this.value)" style="padding:8px 12px">')
+    document.write('<input type="button" class="boton" value="'+ite+'" onclick="muestra(this.value)" style="padding:8px 12px">')
 }
 
 for(let ite of linea2){
-    document.write('<input type="button" class="alpha" value="'+ite+'" onclick="muestra(this.value)" style="padding:8px 12px">')
+    document.write('<input type="button" class="boton" value="'+ite+'" onclick="muestra(this.value)" style="padding:8px 12px">')
 }
 
 for(let ite of linea3){
-    document.write('<input type="button" class="alpha" value="'+ite+'" onclick="muestra(this.value)" style="padding:8px 12px">')
+    document.write('<input type="button" class="boton" value="'+ite+'" onclick="muestra(this.value)" style="padding:8px 12px">')
 }
+
+document.write('<input type="button" class="boton" value="espacio" onclick="espacio()" style="padding:8px 12px">')
+
+document.write('<input type="button" class="boton" value="limpiar" onclick="borrar()" style="padding:8px 12px">')
 
 function muestra(digito){
     pantalla.value +=digito;
-
 }
 
-function limpiar(){
+function borrar(){
     pantalla.value = "";
+}
+
+function espacio(digito){
+    pantalla.value +=" ";
 }
 
 
